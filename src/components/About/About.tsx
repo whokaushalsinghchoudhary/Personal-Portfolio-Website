b7cd2b0667d9c2a3bbc7455040efa0119b6876cb
whokaushalsinghchoudhary <138804392+whokaushalsinghchoudhary@users.noreply.github.com>
2024-09-13 16:52:02 +0530

    Add files via upload

---
+++ src/components/About/About.tsx
@@ -0,0 +1,80 @@
+import { Container } from "./styles";
+import Kaushal from "../../assets/Kaushal.png";
+import htmlIcon from "../../assets/html-icon.svg";
+import cssIcon from "../../assets/css-icon.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+
+
+
+import ScrollAnimation from "react-animate-on-scroll";
+
+export function About() {
+  return (
+    <Container id="about">
+      <div className="about-text">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <h2>About me</h2>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+          <p>
+            Hi there! I'm Kaushal, I developed a strong passion for web development, particularly in frontend technologies like HTML, CSS, and JavaScript.
+          </p>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          <p>
+          I’ve been enhancing my skills in ReactJS, Node.js, Express.js, and MongoDB, working on several projects that allowed me to build and optimize web applications.</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
+          <p>
+            So if you're in need of a new website or just looking to revamp your current online presence, I'd love to chat and see how I can help. Let's bring your website dreams to reality together!</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
+          <h3>Here are my main skills:</h3>
+        </ScrollAnimation>
+        <div className="hard-skills">
+          <div className="hability">
+            
+          </div>
+          <div className="hability">
+            
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <img src={reactIcon} alt="React" />
+            </ScrollAnimation>
+          </div>
+         
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
+              <img src={nodeIcon} alt="Node" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
+              <img src={htmlIcon} alt="Html" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+              <img src={cssIcon} alt="Css" />
+            </ScrollAnimation>
+          </div>
+    
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={jsIcon} alt="JavaScript" />
+            </ScrollAnimation>
+          </div>
+        </div>
+      </div>
+      <div className="about-image">
+        <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
+          <img src={Kaushal} alt="Kaushal" />
+        </ScrollAnimation>
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,29 @@
+import { Container } from "./styles";
+import emailIcon from "../../assets/email-icon.svg";
+import phoneIcon from "../../assets/phone-icon.svg"
+import { Form } from "../Form/Form";
+
+
+export function Contact(){
+
+  return(
+    <Container id="contact">
+      <header>
+        <h2>Contact</h2>
+        <p>Ready to get started on your project? </p>
+        <p>Contact me now for a Free consultation.</p>
+      </header>
+      <div className="contacts">
+        <div>
+        <a href="mailto:kc989770@gmail.com.com"><img src={emailIcon} alt="Email" /></a> 
+          <a href="mailto:kc989770@gmail.com.com">kc989770@gmail.com.com</a>
+        </div>
+        <div>
+        <a href="tel:+919045416523"><img src={phoneIcon} alt="Phone No" /></a>
+          <a href="tel:+919045416523">(+91) 9045416523</a>
+        </div>  
+      </div>
+      <Form></Form>
+    </Container>
+  )
+}
